36ffb4eb3cb8aec3f21136fee05214e6674fd1c3
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-03-26 14:16:25 +0600

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Navbar from "./Navbar"
+
 function App() {
 
   return (
     <>
-      
+      <section>
+        <Navbar />
+      </section>
+      <section id="about">01.About</section>
+      <section id="works">02.Works</section>
+      <section id="experiences">03.Experience</section>
+      <section id="contact">04.Contact</section>
     </>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const Navbar = () => {
+    return (
+        <div className="nav-bar">
+            <div className="logo"><div className="box"><a href="/">N</a></div></div>
+            <div className="links">
+                <a className="nav-links" href="#about"><span>01.</span> About</a>
+                <a className="nav-links" href="#works"><span>02.</span> Works</a>
+                <a className="nav-links" href="#experiences"><span>03.</span> Experiences</a>
+                <a className="nav-links" href="#contact"><span>04.</span> Contact</a>
+                <div className="resume"><div className="r-box"><a className="r-link" href="/">Resume</a></div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
