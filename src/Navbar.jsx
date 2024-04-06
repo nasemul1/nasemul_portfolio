7ea2fa73bff6d15dd9bae0ba8996a71263767659
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-04-07 03:30:08 +0600

    modified src

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
     return (
         <div className="nav-bar">
-            <div className="logo"><div className="box"><a href="/">N</a></div></div>
+            <div className="logo"><div className="box"><a href="https://nasemul1.github.io/nasemul_portfolio/">N</a></div></div>
             <div className="links">
                 <a className="nav-links" href="#skill"><span>01.</span> Skill</a>
                 <a className="nav-links" href="#achievements"><span>02.</span> Achievements</a>
                 <a className="nav-links" href="#projects"><span>03.</span> Projects</a>
                 <a className="nav-links" href="#contact"><span>04.</span> Contact</a>
-                <div className="resume"><div className="r-box"><a className="r-link" href="/">Resume</a></div></div>
+                <div className="resume"><div className="r-box"><a className="r-link" href="https://drive.google.com/file/d/16L3JaEzoPvhAeoHKeJ0zh1PAXQpbOftB/view?usp=sharing">Resume</a></div></div>
             </div>
             <div className="links-drop">
                 <div class="dropdown">
