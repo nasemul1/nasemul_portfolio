ae15b8e847a37df30f92c4e281c54b8a4df0178d
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-05-18 00:33:39 +0600

    modified project page

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -5,6 +5,7 @@ import restuarant from '../assets/restuarent.png'
 import technical from '../assets/technical.png'
 import glass from '../assets/glass.png'
 import blog from '../assets/nasemul-blog.png'
+import animated from '../assets/3d-animated-page.png'
 
 const Projects = () => {
     return (
@@ -16,7 +17,7 @@ const Projects = () => {
                     <a href="">See more</a>
                 </div>
                 <div className="project-container">
-                <div className="proj">
+                    <div className="proj">
                         <img src={blog} alt="blog page image" />
                         <div className="proj-bottom">
                             <h3>Blog Page</h3>
@@ -27,6 +28,21 @@ const Projects = () => {
                             <div className="proj-btn">
                                 <div><a href="https://github.com/nasemul1/nasemul-blog/tree/main" target="_blank"><i class="fa-brands fa-github"></i> Code</a></div>
                                 <div><a href="https://nasemul1.github.io/nasemul-blog/" target="_blank"><i class="fa-solid fa-eye"></i> View</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="proj">
+                        <img src={animated} alt="animated website image" />
+                        <div className="proj-bottom">
+                            <h3>Animated Page</h3>
+                            <div className="proj-tag">
+                                <span>Html</span>
+                                <span>css</span>
+                                <span>Javascript</span>
+                            </div>
+                            <div className="proj-btn">
+                                <div><a href="https://github.com/nasemul1/3d-animated-page" target="_blank"><i class="fa-brands fa-github"></i> Code</a></div>
+                                <div><a href="https://nasemul1.github.io/3d-animated-page/" target="_blank"><i class="fa-solid fa-eye"></i> View</a></div>
                             </div>
                         </div>
                     </div>
